29da7a1a30e9e9283ef6abae8d1e91a13e098d87
Konstantin Weitz <konnew@gmx.de>
2012-03-23 19:26:34 +0100

    Speaking names for variables

--- yali.pl
+++ yali.pl
@@ -1,28 +1,35 @@
 concat([],B,B).
 concat([A|T],B,[A|R]) :- concat(T,B,R).
 
+% N is a list of variables represented as the tuple (id,value)
+% F is a list of functions, represented as the tuple (id,parameters,body)
+% O is the output structure of command
+% Id is an identifier
+% Param is a parameter
+% T stands for the tail of a list
+
 % evaluate a macro
 % eval(N,[M|P],O) :- exp(N,M,P,O). 
 
 % evaluate a function
-eval(N,[F|P],O) :- evalpar(N,P,E), exe(F,E,O).
+eval(N,[Id|Params],O) :- evalpar(N,Params,Evaluated), exe(Id,Evaluated,O).
 
 % resolves a variable, if it is not found, variable name is returned
-eval([[K|V]|_],K,V).
-eval([_|T],K,V) :- eval(T,K,V).
-eval([],K,K).
+eval([[Id|Value]|_],Id,Value).
+eval([_|T],Id,Value) :- eval(T,Id,Value).
+eval([],Id,Id).
 
 % expand a macro
 % exp(N,defun,[I P B],O) :-  
 
 % execute a function
-exe(car,P,O) :- P = [[O|_]].
-exe(cdr,P,O) :- P = [[_|O]].
-exe(list,P,P).
+exe(car,Params,O) :- Params = [[O|_]].
+exe(cdr,Params,O) :- Params = [[_|O]].
+exe(list,Params,Params).
 
 % evaluates all elements of a list
 evalpar(_,[],[]).
-evalpar(N,[A|T],[Q|O]) :- eval(N,A,Q), evalpar(N,T,O).
+evalpar(N,[Element|T],[Evaluated|T2]) :- eval(N,Element,Evaluated), evalpar(N,T,T2).
 
 % eval([.(x,1337)],x,X).
 % eval([],[list,1,2],X).
